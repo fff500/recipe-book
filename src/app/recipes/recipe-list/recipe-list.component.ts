cbd646755847dafee27e1286a21f15df5db96687
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2020-06-14 15:36:44 +0300

    basic components structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Recipe} from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Initial test',
+      'Desc for testing',
+      'https://www.bbcgoodfood.com/sites/default/files/categories/categories-image/2013/05/home-style-lamb-curry.jpg'
+    ),
+    new Recipe(
+      'Second recipe',
+      'Desc #2',
+      'https://assets.rbl.ms/22867002/origin.jpg'
+    ),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Ingredient} from '../shared/ingredient.model';
+
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Orange', 5),
+    new Ingredient('Kiwi', 11),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
